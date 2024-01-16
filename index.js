63a8b660a1d4a6fcd5c1acf2c68be540da1bd08d
Andrew Biron <andrewbiron@andrews-air.home>
2024-01-16 11:02:39 -0500

    added first set of questions

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:'input',
+        name:'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type:'input',
+        name:'description',
+        message: 'What is the description of your project?'
+    },
+    {
+        type:'input',
+        name:'installation',
+        message: 'What are the installation instructions'
+    },
+    {
+        type:'input',
+        name:'usage',
+        message: 'What is the usage information of your project?'
+    },
+    {
+        type:'input',
+        name:'contribution',
+        message: 'What are the contribution guidelines of your project?'
+    },
+    {
+        type:'input',
+        name:'test',
+        message: 'What are the test instructions of your project?'
+    },
+    {
+        type:'list',
+        name:'license',
+        choices: ['MIT', 'ISC', 'Apache license 2.0', 'The Unlicense']
+    },
+    {
+        type:'input',
+        name:'username',
+        message: 'Enter your github username: '
+    },{
+        type:'input',
+        name:'email',
+        message: 'Enter your email: '
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
