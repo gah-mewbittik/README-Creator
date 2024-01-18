7a45164740bbc69e40451f4a5e3e057f8f1e0547
Andrew Biron <andrewbiron@andrews-air.home>
2024-01-18 17:48:21 -0500

    completed the generateMarkdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    return `![Static Badge](https://img.shields.io/badge/License-${license}-blue)`
-  
+  if(license === 'Unlicensed'){
+    return `![Static Badge](https://img.shields.io/badge/License--blue)`;
+
+  }
+  else{
+    return `![Static Badge](https://img.shields.io/badge/License-${license}-blue)`;
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'MIT'){
+    return `The following is a link to the license: https://mit-license.org/`;
+
+  }
+  else if(license === 'ISC'){
+    return `The following is a link to the license: https://www.isc.org/licenses/`;
+  }
+  else if(license === 'Apache'){
+    return `The following is a link to the license: https://www.apache.org/licenses/LICENSE-2.0.html`;
+  }
+  else{
+    return ``;
+  }
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license === 'MIT'){
+    return `Copyright © 2024 <copyright holders>
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+
+  }
+  else if(license === 'ISC'){
+    return `Copyright © 1995-2003 by Internet Software Consortium
+    Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies. 
+    THE SOFTWARE IS PROVIDED “AS IS” AND ISC DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.`;
+  }
+  else if(license === 'Apache'){
+    return `Copyright [2024]
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.`;
+  }
+  else{
+    return ``;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   const renderBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
 
   return `# ${data.title}
 
@@ -47,8 +97,12 @@ function generateMarkdown(data) {
 ## License
   ${data.license}
 
+  ${licenseLink}
+  ${licenseSection}
+
+
 ## Questions
-You can visit my Github profile at: https://github.com/${data.username}.  
+Visit my Github profile at: https://github.com/${data.username}.  
 If you have any questions regarding this application, you can email me at ${data.email}
 
 `;
